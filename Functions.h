960f624e75693cf0d433a2b5867027b7b901f5f3
Максим Данченко <127301792+GrimFoxRF@users.noreply.github.com>
2023-08-30 16:29:59 +0300

    добавил файлы проекта

---
+++ Functions.h
@@ -0,0 +1,95 @@
+﻿#pragma once
+
+struct Node
+{
+    int value;
+    Node* next;
+    std::mutex* node_mutex;
+
+    Node(int val) : value(val), next(nullptr), node_mutex(new std::mutex) {}
+    ~Node() 
+    { 
+        delete node_mutex;
+    }
+};
+
+class FineGrainedQueue
+{
+private:
+    Node* head;
+    std::mutex* queue_mutex;
+
+public:
+    FineGrainedQueue() : head(nullptr), queue_mutex(new std::mutex) {}
+
+    ~FineGrainedQueue()
+    {
+        while (head)
+        {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+        delete queue_mutex;
+    }
+    //вставка элемента в очередь
+    void insertIntoMiddle(int value, int pos)
+    {
+        Node* newNode = new Node(value);
+        queue_mutex->lock();//блокировка очереди
+        Node* curr = head;//текущий узел
+        Node* prev = nullptr;//предыдущий узел
+
+        //обработка пустого списка или вставки в начало
+        if (!curr || pos <= 0)
+        {
+            newNode->next = head;
+            head = newNode;
+            queue_mutex->unlock();//очередь разблокирована
+            return;
+        }
+        //находим нужную позицию
+        while (pos > 0 && curr)
+        {
+            prev = curr;
+            curr = curr->next;
+            --pos;//цикл работает пока счетчик позиции не станет равен 0
+        }
+        queue_mutex->unlock();//очередь разблокирована
+        //вставка нового узла в очередь
+        if (prev)
+        {
+            prev->node_mutex->lock();//блокируем предыдущий узел
+            prev->next = newNode;//указатель на новый узел как next
+            newNode->next = curr;//устанавливаем новому узлу указатель на текущий, как на следующий
+            prev->node_mutex->unlock();//разблокируем предыдущий узел
+        }
+        //если предыдущего узла нет, вставляем новый узел в начало
+        else
+        {
+            if (head) { queue_mutex->lock(); }
+            newNode->next = head;//указатель next теперь указывает на первый элемент очереди
+            head = newNode;//новый узел теперь стал первым элементом
+            if (head) { queue_mutex->unlock(); }
+        }
+    }
+    //вывод содержимого очереди на экран
+    void showQueue()
+    {
+        queue_mutex->lock();
+        Node* curr = head;
+
+        std::cout << "\nОчередь: ";
+
+        while (curr)
+        {
+            curr->node_mutex->lock();
+            std::cout << curr->value << " ";
+            curr->node_mutex->unlock();
+            curr = curr->next;
+        }
+
+        queue_mutex->unlock();
+        std::cout << std::endl;
+    }
+};
---
+++ Main.cpp
@@ -0,0 +1,41 @@
+﻿//вставка элементов в очередь с мелкогранулярной блокировкой
+#include <iostream>
+#include <mutex>
+#include <Windows.h>
+#include <sstream>
+#include "Functions.h"
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "Russian");
+
+    FineGrainedQueue queue;
+    //заполняем очередь первыми 3 элементами
+    queue.insertIntoMiddle(0, 0);
+    queue.insertIntoMiddle(7, 1);
+    queue.insertIntoMiddle(1, 2);
+    queue.showQueue();//выводим очередь на экран
+
+    int value = 0;
+    int npos = 0;
+
+    while (true) //бесконечный цикл
+    {
+        std::cout << "\nВведите значение и номер позиции для вставки:" << std::endl;
+        std::string input;
+        std::getline(std::cin, input);
+
+        std::stringstream stream(input);
+        if (!(stream >> value >> npos))//если некорректный ввод
+        {
+            std::cout << "\nОШИБКА: Некорректный ввод." << std::endl;
+            continue;
+        }
+        queue.insertIntoMiddle(value, npos);//вставка значения в указанную позицию
+        queue.showQueue();//выводим очередь на экран
+    }
+    
+    return 0;
+}
